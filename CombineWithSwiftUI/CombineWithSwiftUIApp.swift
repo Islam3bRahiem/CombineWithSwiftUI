fc96f7bc0c5629ad38b89dd7d79e129577301bed
Islam 3bRa7iem <109504839+IslamRahiem@users.noreply.github.com>
2022-12-02 13:29:41 +0200

    Initial Commit

---
+++ CombineWithSwiftUI/CombineWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineWithSwiftUIApp.swift
+//  CombineWithSwiftUI
+//
+//  Created by Islam Rahiem on 02/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CombineWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
